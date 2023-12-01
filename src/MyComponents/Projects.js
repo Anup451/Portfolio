0b4aba0d72d8f4c3276b1b3006fe45cfc6de4974
Anup451 <anupjujagar121@gmail.com>
2023-12-01 13:13:22 +0530

    added json information and linked to it

--- src/MyComponents/Projects.js
+++ src/MyComponents/Projects.js
@@ -3,6 +3,8 @@ import  Navbar  from './Navbar'
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { Button} from 'react-bootstrap';
+import projectdata from "../Assets/Projects.json";
 function Projects() {
   return (
     <>
@@ -11,16 +13,21 @@ function Projects() {
 
     <div className="container">
     <Row xs={1} md={3} className="g-4" style={{ alignItems:"center",justifyContent:"center", marginTop:"100px"}}>
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col key={idx}>
+      {projectdata.map((project) => (
+        <Col key={project.id}>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+            {/* <Image src="https://user-images.githubusercontent.com/22856752/226000675-0b6fbc9b-2c45-4c8a-ac84-f6188b2cebcb.png" alt={`project ${project.id}`} fluid /> */}
+            {console.log(project.image)}
             <Card.Body>
-              <Card.Title>WRYD</Card.Title>
+              <Card.Title>{project.name}</Card.Title>
               <Card.Text>
-                3D location Tracking application
+                {project.description}
               </Card.Text>
+              <a className="nav-link2" style={{color:"black"}} href="#">Resume</a>
             </Card.Body>
+            {/* <Button style={{width:"100px !important;"}}> Link </Button> */}
+           
           </Card>
         </Col>
       ))}
